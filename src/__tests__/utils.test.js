5ffd8e9dd1670b2dcc3c2e20360af0d2c3a6cd00
Kariuki Kihikah <kariukikihikah@gmail.com>
2025-04-23 20:43:15 +0300

    tests

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,11 @@
-// Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+  it("returns true for a simple palindrome like 'racecar'", () => {
+    expect(isPalindrome("racecar")).toBe(true);
+  });
+
+  it("returns false for a non-palindrome like 'car'", () => {
+    expect(isPalindrome("car")).toBe(false);
+  });
+});
